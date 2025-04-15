cabe998bf23a24d8549ac1b496c343cc48c6f5f1
jmarqb <jacmelbalu7@gmail.com>
2025-04-15 00:58:49 -0400

    chore: add OpenApiConfig

---
+++ ms-auth/src/main/java/com/ms/auth/infrastructure/config/OpenAPIConfig.java
@@ -0,0 +1,32 @@
+package com.ms.auth.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+
+@Configuration
+public class OpenAPIConfig {
+
+	@Bean
+	OpenAPI customOpenAPI(
+	) {
+		return new OpenAPI()
+			.components(new Components()
+				.addSecuritySchemes("bearerAuth",
+					new SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")))
+
+			.info(new Info()
+				.title("Ms-Auth API Documentation")
+				.version("1.0.0")
+				.description("API Documentation."));
+	}
+}
+
