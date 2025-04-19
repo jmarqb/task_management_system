266b45fff7b2d734edb5938aa1ef45acf4ce3ecf
jmarqb <jacmelbalu7@gmail.com>
2025-04-19 12:37:40 -0400

    test: add initial data

---
+++ ms-project-core/src/test/java/com/jmarqb/ms/project/core/MsProjectCoreApplicationTests.java
@@ -0,0 +1,9 @@
+package com.jmarqb.ms.project.core;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProjectCoreApplicationTests {
+
+
+}
---
+++ ms-project-core/src/test/java/com/jmarqb/ms/project/core/data/util/Util.java
@@ -0,0 +1,16 @@
+package com.jmarqb.ms.project.core.data.util;
+
+public class Util {
+
+	public static String projectUId = "11111111-1111-1111-1111-111111111111";
+	public static String projectUserUId = "11111111-0001-0011-0001-111111111111";
+	public static String taskUid = "22222222-2222-2222-2222-222222222222";
+
+	public static Long userId = 1L;
+	public static Long userMemberId = 25L;
+
+	public static String taskNotExist = "33333333-3333-3333-3333-333333333333";
+	public static String projectNotExist = "33333333-2222-0000-1111-333333333333";
+
+
+}
