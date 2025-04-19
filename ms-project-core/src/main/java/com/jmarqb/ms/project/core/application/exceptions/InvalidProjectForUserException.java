012c4098c29c3ccc64049098e64fdd53f3872886
jmarqb <jacmelbalu7@gmail.com>
2025-04-19 12:19:03 -0400

    feat: add exceptions to application layer

---
+++ ms-project-core/src/main/java/com/jmarqb/ms/project/core/application/exceptions/InvalidProjectForUserException.java
@@ -0,0 +1,7 @@
+package com.jmarqb.ms.project.core.application.exceptions;
+
+public class InvalidProjectForUserException extends RuntimeException {
+	public InvalidProjectForUserException(String message) {
+		super(message);
+	}
+}
---
+++ ms-project-core/src/main/java/com/jmarqb/ms/project/core/application/exceptions/ProjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jmarqb.ms.project.core.application.exceptions;
+
+public class ProjectNotFoundException extends RuntimeException {
+	public ProjectNotFoundException(String message) {
+		super(message);
+	}
+}
