8bafbec6c1491f96ccd4260a2c504922027d5503
jmarqb <jacmelbalu7@gmail.com>
2025-04-22 17:31:01 -0400

    chore: add personalized pagination

--- ms-project-core/src/main/java/com/jmarqb/ms/project/core/domain/ports/output/persistence/ProjectPersistencePort.java
+++ ms-project-core/src/main/java/com/jmarqb/ms/project/core/domain/ports/output/persistence/ProjectPersistencePort.java
@@ -1,16 +1,15 @@
 package com.jmarqb.ms.project.core.domain.ports.output.persistence;
 
-import org.springframework.data.domain.Pageable;
+import com.jmarqb.ms.project.core.domain.model.Pagination;
+import com.jmarqb.ms.project.core.domain.model.Project;
 
 import java.util.List;
-
-import com.jmarqb.ms.project.core.domain.model.Project;
 
 public interface ProjectPersistencePort {
 
 	Project save(Project project);
 
-	List<Project> searchAll(Pageable pageable, Long userId);
+	List<Project> searchAll(Pagination pagination, Long userId);
 
 	Project findByUid(String uid);
 
